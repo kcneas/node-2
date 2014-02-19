b5c46a1623987310ae944b0f7b975a21d1898f39
OpenShift guest <kcneas0807@gmail.com>
2014-02-19 01:25:37 -0500

    mobile css add

--- web.js
+++ web.js
@@ -10,21 +10,8 @@ app.use('/images', express.static(__dirname + "/images"));
 app.use('/font', express.static(__dirname + "/font"));
 // 라우팅 설정
 app.get('/', function (req, res) { 
-    fs.readFile('index.html', function (error, data) { // index.html 파일 로드 .
-        if (error) {
-         console.log(error);
-        } else {
-            res.writeHead(200, { 'Content-Type': 'text/html' }); // Head Type 설정 .
-            res.end(data); // 로드 html response .
-            var forwardedIpsStr = req.header('x-forwarded-for');
-           var IP = '';
-           
-           if (forwardedIpsStr) {
-              IP = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-              console.log(IP + "  count : " + count++);
-           }
-        } 
-    });
+    var stream = fs.createReadStream('index.html');
+    stream.pipe(res);
 });
 
 
